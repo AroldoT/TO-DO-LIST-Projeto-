80e0713df99e7c276fb2335ef108d3810b829c3c
Aroldo Teixeira <aroldoteixeira66@gmail.com>
2024-06-02 15:09:29 -0300

    Versão nova 1.0.1
    Verificar o que falta para mapear as várias classes e add uma classe sumir aos que não são daquele dia da semana.

--- index.js
+++ index.js
@@ -1,15 +1,97 @@
+
 document.addEventListener('DOMContentLoaded', () =>{
   const newTaskInput = document.querySelector('#new-task');
   const addTaskBtn = document.querySelector('#add-task-btn');
+
+  const lateral = document.querySelector('aside > h2')
+  lateral.addEventListener('click', () => {
+  })
+
   const taskList = document.querySelector('#task-list');
-
-    const addTask = () => {/* */
-      const textTask = newTaskInput.value.trim();
-      if (textTask !== '') {
-        const li = document.createElement('li');
-
-        li.innerHTML = `<span>${textTask}</span><button class="delete-btn">Delete</button>`;
-        taskList.appendChild(li);
+    const addTask = () => {/*Fução para executar a adição de tarefas */
+      const textTask = newTaskInput.value.trim(); 
+      if (textTask !== '') { /** se a nova tarefa não estiver vazia */
+        const li = document.createElement('li'); /*Ela deve criar um elemento li e como o documento de html reconhece as tags o proprio documento edita para que <li>*/
+        //const lista = document.querySelectorAll('li')
+        if (lateral.id == 10){
+          li.innerHTML = `<span class="${lateral.classList}">${textTask}</span><button class="delete-btn">Delete</button>`;
+           
+          for (item of taskList){
+            if (li.classList != 'segunda') {
+              li.style.display = 'none'
+            }
+            else if (li.classList == 'segunda') {
+              li.style.display = 'flex'
+            };
+          }
+        }
+        else if (lateral.id == 20){
+          li.innerHTML = `<span class="${lateral.classList}">${textTask}</span><button class="delete-btn">Delete</button>`;
+          for (item of taskList){
+            if (li.classList != 'terça') {
+              li.style.display = 'none'
+            }
+            else if (li.classList == 'terça') {
+              li.style.display = 'flex'
+            }
+          };
+        }
+        else if (lateral.id == 30){
+          li.innerHTML = `<span class="${lateral.classList}">${textTask}</span><button class="delete-btn">Delete</button>`;
+          for (item of taskList){
+            if (li.classList != 'quarta') {
+              li.style.display = 'none'
+            }
+            else if (li.classList == 'quarta') {
+              li.style.display = 'flex'
+            }
+          };
+        }
+        else if (lateral.id == 40){
+          li.innerHTML = `<span class="${lateral.classList}">${textTask}</span><button class="delete-btn">Delete</button>`;
+          for (item of taskList){
+            if (li.classList != 'quinta') {
+              li.style.display = 'none'
+            }
+            else if (li.classList == 'quinta') {
+              li.style.display = 'flex'
+            }
+          };
+        }
+        else if (lateral.id == 50){
+          li.innerHTML = `<span class="${lateral.classList}">${textTask}</span><button class="delete-btn">Delete</button>`;
+          for (item of taskList){
+            if (li.classList != 'sexta') {
+              li.style.display = 'none'
+            }
+            else if (li.classList == 'sexta') {
+              li.style.display = 'flex'
+            }
+          };
+        }
+        else if (lateral.id == 60){
+          li.innerHTML = `<span class="${lateral.classList}">${textTask}</span><button class="delete-btn">Delete</button>`;
+          for (item of taskList){
+            if (li.classList != 'sabado') {
+              li.style.display = 'none'
+            }
+            else if (li.classList == 'sabado') {
+              li.style.display = 'flex'
+            }
+          };
+        }
+        else if (lateral.id == 70){
+          li.innerHTML = `<span class="${lateral.classList}">${textTask}</span><button class="delete-btn">Delete</button>`;
+          for (item of taskList){
+            if (li.classList != 'domingo') {
+              li.style.display = 'none'
+            }
+            else if (li.classList == 'domingo') {
+              li.style.display = 'flex'
+            }
+          };
+        }
+          taskList.appendChild(li);
 
         li.querySelector('.delete-btn').addEventListener('click', () => {
           li.remove();
@@ -29,4 +111,6 @@ document.addEventListener('DOMContentLoaded', () =>{
         addTask();
       }
     })
-})
+});
+
+
